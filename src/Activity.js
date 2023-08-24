151d9c374091b360516e782456ba096c21dc687a
“KseGreb” <ksegreb@gmail.com>
2023-08-24 08:15:40 -0600

    changes in activity

--- src/Activity.js
+++ src/Activity.js
@@ -1,6 +1,25 @@
+import { useEffect, useState } from 'react';
+
+function Activity(){
+  const [advice, setAdvice] = useState("");
+  const [showAdvice, setShowAdvice] = useState("")
+  
+
+  useEffect(()=>{
+    const FindAdvice = async () =>{
+      const response = await fetch (`http://www.boredapi.com/api/activity/`);
+      const data = await response.json();
+      console.log(data.activity);
+      setAdvice(data.activity)
+    }
+    FindAdvice()
+  },[showAdvice])
 
 
-function Activity({oneProp, twoProp}){
+function handleSubmit(e) {
+  e.preventDefault();
+  setShowAdvice(advice);
+}
   return(
     <div className='main'>
             
@@ -8,9 +27,9 @@ function Activity({oneProp, twoProp}){
         <h1>Are you bored?</h1>
         <p>Press the button and get an idea of what to do!</p>
         
-        <form onSubmit={twoProp}>
+        <form onSubmit={handleSubmit}>
           <button>WHAT SHALL I DO TODAY?</button>
-          <h2>{oneProp}</h2>
+          <h2>{showAdvice}</h2>
         </form>
       </div>
 
--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-
-import { useEffect, useState } from 'react';
 import './App.css';
 import Activity from './Activity';
-import VideoBg from './mainScreen';
+import VideoBg from './VideoBg';
 
 
 function App() {
-
-  const [advice, setAdvice] = useState("");
-  const [showAdvice, setShowAdvice] = useState("")
-  
-
-  useEffect(()=>{
-    const FindAdvice = async () =>{
-      const response = await fetch (`http://www.boredapi.com/api/activity/`);
-      const data = await response.json();
-      console.log(data.activity);
-      setAdvice(data.activity)
-    }
-    FindAdvice()
-  },[showAdvice])
-
-
-function handleSubmit(e) {
-  e.preventDefault();
-  setShowAdvice(advice);
-}
 
   return (
                                                                                                                                                                                                                                                                                                                    
     <div className='main'>
       
-      <Activity oneProp={showAdvice} twoProp={handleSubmit}/>
+      <Activity/>
       <VideoBg/>
       
     </div>
